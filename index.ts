6392aef2e09edff2074e34e5fee68d67a81cefb8
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-02 18:52:53 -0300

    🎉 feat: Initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+const device = {
+  desktop: "only screen and (max-width: 2560px)",
+  laptopL: "only screen and (max-width: 1440px)",
+  laptop: "only screen and (max-width: 1024px)",
+  tablet: "only screen and (max-width: 768px)",
+  mobileL: "only screen and (max-width: 425px)",
+  mobileM: "only screen and (max-width: 375px)",
+  mobileS: "only screen and (max-width: 320px)",
+};
+
+export default device;
