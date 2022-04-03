5dd146e80bd8742f19f8c7b14edba14f7e35e34d
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-02 22:42:31 -0300

    ✨ feat: Add new ```index.ts```

--- index.ts
+++ index.ts
@@ -1,11 +1,41 @@
-const device = {
-  desktop: "only screen and (max-width: 2560px)",
-  laptopL: "only screen and (max-width: 1440px)",
-  laptop: "only screen and (max-width: 1024px)",
-  tablet: "only screen and (max-width: 768px)",
-  mobileL: "only screen and (max-width: 425px)",
-  mobileM: "only screen and (max-width: 375px)",
-  mobileS: "only screen and (max-width: 320px)",
+interface DeviceOptions {
+  desktop: string;
+  laptopL: string;
+  laptop: string;
+  tablet: string;
+  mobileL: string;
+  mobileM: string;
+  mobileS: string;
+}
+
+interface Breakpoints {
+  desktop: number;
+  laptopL: number;
+  laptop: number;
+  tablet: number;
+  mobileL: number;
+  mobileM: number;
+  mobileS: number;
+}
+
+const size: Breakpoints = {
+  desktop: 2560,
+  laptopL: 1440,
+  laptop: 1024,
+  tablet: 768,
+  mobileL: 425,
+  mobileM: 375,
+  mobileS: 320,
+};
+
+const device: DeviceOptions = {
+  desktop: `(max-width: ${size.desktop}px)`,
+  laptopL: `(max-width: ${size.laptopL}px)`,
+  laptop: `(max-width: ${size.laptop}px)`,
+  tablet: `(max-width: ${size.tablet}px)`,
+  mobileL: `(max-width: ${size.mobileL}px)`,
+  mobileM: `(max-width: ${size.mobileM}px)`,
+  mobileS: `(max-width: ${size.mobileS}px)`,
 };
 
 export default device;
